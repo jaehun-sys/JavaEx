341b270826c367335ceb2573e2f48471509a68f2
KOSTA <KOSTA@DESKTOP-SN6M77H>
2021-12-08 16:45:29 +0900

    2021-12-08-v5 git캐시지우고 다시 ignore

---
+++ src/com/javaex/problem03/Print.java
@@ -0,0 +1,20 @@
+package com.javaex.problem03;
+
+public class Print {
+	public void printer(int val){
+        System.out.println(val);
+    }
+
+    //메소드  작성
+	public void printer(boolean B) {
+		System.out.println(B);
+	}
+	
+	public void printer(double d) {
+		System.out.println(d);
+	}
+	
+	public void printer(String name) {
+		System.out.println(name);
+	}
+}
