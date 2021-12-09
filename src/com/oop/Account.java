af5922fb004fce0b0bd7df27e817d40b2b0b1ae4
KOSTA <us03098@gmail.com>
2021-12-09 10:16:45 +0900

    2021-12-09-v2 Account클래스

---
+++ src/com/oop/Account.java
@@ -0,0 +1,31 @@
+package com.oop;
+
+public class Account {
+	@SuppressWarnings("unused")
+	private String accountNo;
+    private int balance;
+
+    //생성자 작성
+	public Account(String accountNo) {
+		super();
+		this.accountNo = accountNo;
+		System.out.println(this.accountNo + " 계좌가 개설되었습니다.");
+	}
+
+	//필요한 메소드 작성
+	public void deposit(int money) {
+		this.balance += money;
+	}
+
+	public void withdraw(int money) {
+		if(money > this.balance) {
+			System.out.println("잔액이 부족합니다.");
+		}else {
+			this.balance -= money;
+		}
+	}
+
+	public void showBalance() {
+		System.out.println(balance);
+	}
+}
